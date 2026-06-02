1f7e893a3f0fa49660dd826a063d531944f054cf
Paulina Czajkowska <paulina.czajkowska04@gmail.com>
2026-06-02 22:34:43 +0200

    add: `RequestInfo`

--- python/scylla/policies/__init__.py
+++ python/scylla/policies/__init__.py
@@ -2,7 +2,15 @@ from .address_translator import AddressTranslator, DictAddressTranslator, Untran
 from .authenticator_provider import Authenticator, AuthenticatorProvider
 from .host_filter import AcceptAllHostFilter, AllowListHostFilter, DcHostFilter, HostFilter, Peer
 from .load_balancing import DefaultPolicy, LoadBalancingPolicy, NodeLocationPreference, RoutingInfo
-from .retry_policy import CqlResponseKind, DbError, OperationType, RequestAttemptError, RetryDecision, WriteType
+from .retry_policy import (
+    CqlResponseKind,
+    DbError,
+    OperationType,
+    RequestAttemptError,
+    RequestInfo,
+    RetryDecision,
+    WriteType,
+)
 from .timestamp_generator import MonotonicTimestampGenerator, SimpleTimestampGenerator, TimestampGenerator
 
 __all__ = [
@@ -23,6 +31,7 @@ __all__ = [
     "OperationType",
     "Peer",
     "RequestAttemptError",
+    "RequestInfo",
     "RetryDecision",
     "RoutingInfo",
     "SimpleTimestampGenerator",
