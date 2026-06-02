c3dfff74c7e13b07d457e687cabb3e04c85f4444
Paulina Czajkowska <paulina.czajkowska04@gmail.com>
2026-06-02 22:43:19 +0200

    add: retry policies and retry sessions

---
+++ src/policies/retry/policies.rs
@@ -0,0 +1,231 @@
+use crate::policies::retry::decision::PyRetryDecision;
+use crate::policies::retry::request::PyRequestInfo;
+use pyo3::intern;
+use pyo3::prelude::*;
+use pyo3::sync::MutexExt;
+use scylla::policies::retry::{
+    DefaultRetryPolicy, DefaultRetrySession, DowngradingConsistencyRetryPolicy,
+    FallthroughRetryPolicy, RequestInfo, RetryDecision, RetryPolicy, RetrySession,
+};
+use scylla::policies::retry::{DowngradingConsistencyRetrySession, FallthroughRetrySession};
+use std::sync::Arc;
+use std::sync::Mutex;
+use tracing::error;
+
+#[allow(dead_code)]
+#[derive(Debug, Clone)]
+pub(crate) struct PyCustomRetrySession {
+    pub(crate) inner: Py<PyAny>,
+}
+
+impl RetrySession for PyCustomRetrySession {
+    fn decide_should_retry(&mut self, request_info: RequestInfo) -> RetryDecision {
+        Python::attach(|py| {
+            let py_retry_session = self.inner.bind(py);
+            let py_request_info = PyRequestInfo::from(&request_info);
+
+            let result = py_retry_session
+                .call_method1(intern!(py, "decide_should_retry"), (py_request_info,));
+
+            match result {
+                Ok(res) => match res.cast::<PyRetryDecision>() {
+                    Ok(py_retry_decision) => RetryDecision::from(py_retry_decision.get()),
+                    Err(err) => {
+                        error!(
+                            "Failed to extract 'PyRetryDecision'. \
+                            Fallback action: 'DontRetry'. Reason: {}",
+                            err
+                        );
+                        RetryDecision::DontRetry
+                    }
+                },
+                Err(err) => {
+                    error!(
+                        "Failed to call decide_should_retry() on custom retry session. \
+                        Fallback action: 'DontRetry'. Reason: {}",
+                        err
+                    );
+                    RetryDecision::DontRetry
+                }
+            }
+        })
+    }
+
+    fn reset(&mut self) {
+        Python::attach(|py| {
+            let obj = self.inner.bind(py);
+
+            if let Err(err) = obj.call_method0(intern!(py, "reset")) {
+                error!(
+                    "Failed to call reset() on custom retry session. \
+                    Reason: {}",
+                    err
+                );
+            }
+        })
+    }
+}
+
+#[pyclass(name = "DefaultRetrySession", frozen)]
+pub(crate) struct PyDefaultRetrySession {
+    pub(crate) inner: Arc<Mutex<DefaultRetrySession>>,
+}
+
+#[pymethods]
+impl PyDefaultRetrySession {
+    #[new]
+    fn py_new() -> Self {
+        Self {
+            inner: Arc::new(Mutex::new(DefaultRetrySession::new())),
+        }
+    }
+
+    fn decide_should_retry(&self, request_info: PyRequestInfo, py: Python<'_>) -> PyRetryDecision {
+        let mut inner = self.inner.lock_py_attached(py).unwrap();
+        inner
+            .decide_should_retry(request_info.to_request_info())
+            .into()
+    }
+
+    fn reset(&self, py: Python<'_>) {
+        let mut inner = self.inner.lock_py_attached(py).unwrap();
+        inner.reset();
+    }
+}
+
+#[pyclass(name = "DowngradingConsistencyRetrySession", frozen)]
+pub(crate) struct PyDowngradingConsistencyRetrySession {
+    pub(crate) inner: Arc<Mutex<DowngradingConsistencyRetrySession>>,
+}
+
+#[pymethods]
+impl PyDowngradingConsistencyRetrySession {
+    #[new]
+    fn py_new() -> Self {
+        Self {
+            inner: Arc::new(Mutex::new(DowngradingConsistencyRetrySession::new())),
+        }
+    }
+
+    fn decide_should_retry(&self, request_info: PyRequestInfo, py: Python<'_>) -> PyRetryDecision {
+        let mut inner = self.inner.lock_py_attached(py).unwrap();
+        inner
+            .decide_should_retry(request_info.to_request_info())
+            .into()
+    }
+
+    fn reset(&self, py: Python<'_>) {
+        let mut inner = self.inner.lock_py_attached(py).unwrap();
+        inner.reset();
+    }
+}
+
+#[pyclass(name = "FallthroughRetrySession", frozen)]
+pub(crate) struct PyFallthroughRetrySession {}
+
+#[pymethods]
+impl PyFallthroughRetrySession {
+    #[new]
+    fn py_new() -> Self {
+        Self {}
+    }
+
+    #[allow(unused_variables)]
+    fn decide_should_retry(&self, request_info: PyRequestInfo) -> PyRetryDecision {
+        PyRetryDecision::DontRetry()
+    }
+
+    fn reset(&self) {}
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub(crate) struct PyCustomRetryPolicy {
+    pub(crate) inner: Py<PyAny>,
+}
+
+impl RetryPolicy for PyCustomRetryPolicy {
+    fn new_session(&self) -> Box<dyn RetrySession> {
+        Python::attach(|py| -> Box<dyn RetrySession> {
+            let policy = self.inner.bind(py);
+
+            match policy.call_method0(intern!(py, "new_session")) {
+                Ok(session) => Box::new(PyCustomRetrySession {
+                    _inner: session.unbind(),
+                }),
+                Err(err) => {
+                    error!(
+                        "Failed to call new_session() on custom retry policy. \
+                        Fallback action: 'DefaultRetrySession'. Reason: {}",
+                        err
+                    );
+                    Box::new(DefaultRetrySession::new())
+                }
+            }
+        })
+    }
+}
+
+#[pyclass(name = "DefaultRetryPolicy", frozen)]
+#[derive(Debug)]
+pub(crate) struct PyDefaultRetryPolicy {
+    pub(crate) inner: Arc<DefaultRetryPolicy>,
+}
+
+#[pymethods]
+impl PyDefaultRetryPolicy {
+    #[new]
+    fn py_new() -> Self {
+        Self {
+            inner: Arc::new(DefaultRetryPolicy::new()),
+        }
+    }
+
+    fn new_session(&self) -> PyDefaultRetrySession {
+        PyDefaultRetrySession {
+            inner: Arc::new(Mutex::new(DefaultRetrySession::new())),
+        }
+    }
+}
+
+#[pyclass(name = "DowngradingConsistencyRetryPolicy", frozen)]
+#[derive(Debug)]
+pub(crate) struct PyDowngradingConsistencyRetryPolicy {
+    pub(crate) inner: Arc<DowngradingConsistencyRetryPolicy>,
+}
+
+#[pymethods]
+impl PyDowngradingConsistencyRetryPolicy {
+    #[new]
+    fn py_new() -> Self {
+        Self {
+            inner: Arc::new(DowngradingConsistencyRetryPolicy::new()),
+        }
+    }
+
+    fn new_session(&self) -> PyDowngradingConsistencyRetrySession {
+        PyDowngradingConsistencyRetrySession {
+            inner: Arc::new(Mutex::new(DowngradingConsistencyRetrySession::new())),
+        }
+    }
+}
+
+#[pyclass(name = "FallthroughRetryPolicy", frozen)]
+#[derive(Debug)]
+pub(crate) struct PyFallthroughRetryPolicy {
+    pub(crate) inner: Arc<FallthroughRetryPolicy>,
+}
+
+#[pymethods]
+impl PyFallthroughRetryPolicy {
+    #[new]
+    fn py_new() -> Self {
+        Self {
+            inner: Arc::new(FallthroughRetryPolicy::new()),
+        }
+    }
+
+    fn new_session(&self) -> PyFallthroughRetrySession {
+        PyFallthroughRetrySession {}
+    }
+}
